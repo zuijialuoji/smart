bf0fac164bd1fb455a85b6090b1de683c82370eb
崔宗鲁 <524464317@qq.com>
2018-05-21 15:26:09 +0800

    还原

---
+++ samrt-customer/src/main/java/com/smart/SmartCustomerApplication.java
@@ -0,0 +1,18 @@
+package com.smart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@ServletComponentScan("com.smart.*")
+@MapperScan("com.smart.dao.customer")
+public class SmartCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCustomerApplication.class, args);
+	}
+}
