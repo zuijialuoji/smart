d309f1d8eaaf62f2cbcc48a7d715325e530842ac
崔宗鲁 <524464317@qq.com>
2018-05-21 13:54:57 +0800

    还原

--- smart-comm/src/main/java/com/smart/core/domin/R.java
+++ smart-comm/src/main/java/com/smart/core/domin/R.java
@@ -1,9 +1,55 @@
 package com.smart.core.domin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Created by 崔宗鲁 on 2018/5/21.
+ * Created by 崔宗鲁 on 2018/5/11.
  *
  * @Description:
  */
-public class R {
+public class R extends HashMap<String, Object> {
+    private static final long serialVersionUID = 1L;
+
+    public R() {
+        put("code", 0);
+        put("msg", "操作成功");
+    }
+
+    public static R error() {
+        return error(1, "操作失败");
+    }
+
+    public static R error(String msg) {
+        return error(500, msg);
+    }
+
+    public static R error(int code, String msg) {
+        R r = new R();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R ok(String msg) {
+        R r = new R();
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R ok(Map<String, Object> map) {
+        R r = new R();
+        r.putAll(map);
+        return r;
+    }
+
+    public static R ok() {
+        return new R();
+    }
+
+    @Override
+    public R put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
 }
